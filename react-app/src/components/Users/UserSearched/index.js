f51d711ba83c2c16412129dfe9515d54ef2c4112
Murphyn5 <nlimurphy@gmail.com>
2023-03-25 19:53:01 -0700

    created followers table

--- react-app/src/components/Users/UserSearched/index.js
+++ react-app/src/components/Users/UserSearched/index.js
@@ -35,12 +35,16 @@ const UserSearched = () => {
         <h1>Athlete Search</h1>
         <div style={{ lineHeight: "22px", fontWeight: "400", fontSize: "22px" }}>{users.length} {users.length === 1 ? "Athlete" : "Athletes"}</div>
         <hr style={{ marginBottom: "0", borderBottom: "0" }}></hr>
-        {users.map((user) => {
-          count++
-          return (
-            <UserSearchCard user={user} count={count} key={user.id} />
-          );
-        })}
+        {users.length === 0 ?
+          <div>
+            <br></br>
+            <div>No athletes with name <span style={{fontWeight:"bold"}}>{searchString}</span> found</div>
+          </div> : users.map((user) => {
+            count++
+            return (
+              <UserSearchCard user={user} count={count} key={user.id} />
+            );
+          })}
       </div>
     </div>
   );
