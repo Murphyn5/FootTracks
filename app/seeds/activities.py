c57a91d46aa9a73aa0886e727fafe23bd814a570
Murphyn5 <nlimurphy@gmail.com>
2023-03-31 12:56:12 -0700

    added proifile pictures to seed data

--- app/seeds/activities.py
+++ app/seeds/activities.py
@@ -13,6 +13,8 @@ def seed_activities():
                          distance=3.2,
                          duration=1800,
                          calories=300,
+                         activity_date=datetime(
+                             2022, 12, 28, 23, 55, 59, 342380),
                          elevation=50)
     activity2 = Activity(owner_id=2,
                          title='Oyster Dome!',
@@ -22,7 +24,9 @@ def seed_activities():
                          duration=7200,
                          calories=500,
                          elevation=1800,
-                         coordinates ="48.62686,-122.41479;48.62679,-122.41443;48.62610,-122.41414;48.62559,-122.41425;48.62501,-122.41397;48.62448,-122.41365;48.62408,-122.41379;48.62394,-122.41396;48.62374,-122.41446;48.62423,-122.41491;48.62403,-122.41516;48.62439,-122.41527;48.62411,-122.41545;48.62455,-122.41562;48.62440,-122.41595;48.62465,-122.41645;48.62498,-122.41676;48.62481,-122.41677;48.62494,-122.41689;48.62495,-122.41705;48.62476,-122.41698;48.62463,-122.41719;48.62438,-122.41745;48.62403,-122.41752;48.62417,-122.41792;48.62399,-122.41787;48.62372,-122.41841;48.62325,-122.41867;48.62365,-122.41878;48.62390,-122.41913;48.62386,-122.41957;48.62342,-122.41976;48.62304,-122.42026;48.62290,-122.42102;48.62189,-122.42149;48.62124,-122.42202;48.62050,-122.42344;48.61968,-122.42451;48.61863,-122.42536;48.61798,-122.42570;48.61741,-122.42637;48.61683,-122.42664;48.61549,-122.42750;48.61481,-122.42752;48.61434,-122.42767;48.61272,-122.42868;48.61125,-122.42893;48.60988,-122.42898;48.60892,-122.42850;48.60819,-122.42864;48.60892,-122.42959;48.60983,-122.43054;48.61084,-122.43075;48.61192,-122.43112;48.61322,-122.43169;48.61213,-122.43231;48.61075,-122.43260;48.60948,-122.43262;48.60754,-122.43183;48.60800,-122.43260;48.60893,-122.43339"
+                         activity_date=datetime(
+                             2023, 2, 13, 23, 55, 59, 342380),
+                         coordinates="48.62686,-122.41479;48.62679,-122.41443;48.62610,-122.41414;48.62559,-122.41425;48.62501,-122.41397;48.62448,-122.41365;48.62408,-122.41379;48.62394,-122.41396;48.62374,-122.41446;48.62423,-122.41491;48.62403,-122.41516;48.62439,-122.41527;48.62411,-122.41545;48.62455,-122.41562;48.62440,-122.41595;48.62465,-122.41645;48.62498,-122.41676;48.62481,-122.41677;48.62494,-122.41689;48.62495,-122.41705;48.62476,-122.41698;48.62463,-122.41719;48.62438,-122.41745;48.62403,-122.41752;48.62417,-122.41792;48.62399,-122.41787;48.62372,-122.41841;48.62325,-122.41867;48.62365,-122.41878;48.62390,-122.41913;48.62386,-122.41957;48.62342,-122.41976;48.62304,-122.42026;48.62290,-122.42102;48.62189,-122.42149;48.62124,-122.42202;48.62050,-122.42344;48.61968,-122.42451;48.61863,-122.42536;48.61798,-122.42570;48.61741,-122.42637;48.61683,-122.42664;48.61549,-122.42750;48.61481,-122.42752;48.61434,-122.42767;48.61272,-122.42868;48.61125,-122.42893;48.60988,-122.42898;48.60892,-122.42850;48.60819,-122.42864;48.60892,-122.42959;48.60983,-122.43054;48.61084,-122.43075;48.61192,-122.43112;48.61322,-122.43169;48.61213,-122.43231;48.61075,-122.43260;48.60948,-122.43262;48.60754,-122.43183;48.60800,-122.43260;48.60893,-122.43339"
                          )
     activity3 = Activity(owner_id=3,
                          title='Morning Run',
@@ -31,6 +35,8 @@ def seed_activities():
                          distance=5.0,
                          duration=1800,
                          calories=200,
+                         activity_date=datetime(
+                             2023, 1, 14, 23, 55, 59, 342380),
                          elevation=50)
     activity4 = Activity(owner_id=1,
                          title='Afternoon Hike',
@@ -39,6 +45,8 @@ def seed_activities():
                          distance=10.5,
                          duration=7200,
                          calories=600,
+                         activity_date=datetime(
+                             2023, 2, 5, 23, 55, 59, 342380),
                          elevation=500)
     activity5 = Activity(owner_id=2,
                          title='Evening Bike Ride',
@@ -47,6 +55,8 @@ def seed_activities():
                          distance=8.3,
                          duration=2700,
                          calories=300,
+                         activity_date=datetime(
+                             2023, 2, 7, 23, 55, 59, 342380),
                          elevation=70)
     activity6 = Activity(owner_id=3,
                          title='Indoor Ride',
@@ -55,6 +65,8 @@ def seed_activities():
                          distance=0.0,
                          duration=3600,
                          calories=500,
+                         activity_date=datetime(
+                             2023, 3, 1, 23, 55, 59, 342380),
                          elevation=0)
     activity7 = Activity(owner_id=1,
                          title='Evening Jog',
@@ -63,6 +75,8 @@ def seed_activities():
                          distance=3.0,
                          duration=1500,
                          calories=300,
+                         activity_date=datetime(
+                             2023, 1, 18, 23, 55, 59, 342380),
                          elevation=30)
     activity8 = Activity(owner_id=2,
                          title='Morning Walk',
@@ -71,14 +85,18 @@ def seed_activities():
                          distance=2.5,
                          duration=1800,
                          calories=100,
+                         activity_date=datetime(
+                             2022, 12, 21, 23, 55, 59, 342380),
                          elevation=10)
     activity9 = Activity(owner_id=3,
-                         title='Morning Ride',
+                         title='Christmas Morning Ride',
                          description='A scenic morning bike ride',
                          type='Ride',
                          distance=10.0,
                          duration=3600,
                          calories=500,
+                         activity_date=datetime(
+                             2022, 12, 25, 23, 55, 59, 342380),
                          elevation=100)
     activity10 = Activity(owner_id=1,
                           title='Afternoon Run',
@@ -87,6 +105,8 @@ def seed_activities():
                           distance=5.0,
                           duration=2400,
                           calories=400,
+                          activity_date=datetime(
+                              2023, 1, 19, 23, 55, 59, 342380),
                           elevation=50)
     activity11 = Activity(owner_id=2,
                           title='Evening Ride',
@@ -95,6 +115,8 @@ def seed_activities():
                           distance=8.0,
                           duration=2700,
                           calories=350,
+                          activity_date=datetime(
+                              2023, 3, 18, 23, 55, 59, 342380),
                           elevation=80)
     activity12 = Activity(owner_id=3,
                           title='Morning Run',
@@ -103,6 +125,8 @@ def seed_activities():
                           distance=3.0,
                           duration=1500,
                           calories=200,
+                          activity_date=datetime(
+                              2023, 3, 2, 23, 55, 59, 342380),
                           elevation=20)
 
     activity13 = Activity(owner_id=1,
@@ -112,14 +136,18 @@ def seed_activities():
                           distance=20.0,
                           duration=7200,
                           calories=1000,
+                          activity_date=datetime(
+                              2023, 1, 29, 23, 55, 59, 342380),
                           elevation=200)
     activity14 = Activity(owner_id=2,
-                          title='Morning Run',
+                          title='Valentines Day Morning Run',
                           description='A peaceful morning run in the park',
                           type='Run',
                           distance=4.0,
                           duration=1800,
                           calories=250,
+                          activity_date=datetime(
+                              2023, 2, 14, 23, 55, 59, 342380),
                           elevation=30)
     activity15 = Activity(owner_id=3,
                           title='Evening Ride',
@@ -128,6 +156,8 @@ def seed_activities():
                           distance=15.0,
                           duration=5400,
                           calories=700,
+                          activity_date=datetime(
+                              2023, 3, 14, 23, 55, 59, 342380),
                           elevation=150)
     activity16 = Activity(owner_id=1,
                           title='Afternoon Run',
@@ -136,6 +166,8 @@ def seed_activities():
                           distance=7.0,
                           duration=3600,
                           calories=600,
+                          activity_date=datetime(
+                              2023, 3, 5, 23, 55, 59, 342380),
                           elevation=400)
 
     db.session.add_all([
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -6,9 +6,9 @@ from datetime import date
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     users = [
-    User(first_name='John', last_name='Doe', email='john.doe@example.com', password='password', gender='male', birthday=date(1990, 1, 1)),
-    User(first_name='Marnie', last_name='Smith', email='marnie@aa.io', password='password', gender='female', birthday=date(1995, 5, 5)),
-    User(first_name='Bobbie', last_name='Smith', email='bobbie@aa.io', password='password', gender='male', birthday=date(1980, 12, 31))
+    User(first_name='John', last_name='Doe', email='john.doe@example.com', password='password', gender='male', profile_picture="https://nm-aws-pern-demo.s3.us-west-2.amazonaws.com/0d1d13dd41004b879bdcf3bfdc861246.png", birthday=date(1990, 1, 1)),
+    User(first_name='Marnie', last_name='Smith', email='marnie@aa.io', password='password', gender='female', profile_picture="https://nm-aws-pern-demo.s3.us-west-2.amazonaws.com/fb9791c81a064178a7cb47e71d10bb65.png", birthday=date(1995, 5, 5)),
+    User(first_name='Bobbie', last_name='Smith', email='bobbie@aa.io', password='password', gender='male', profile_picture="https://nm-aws-pern-demo.s3.us-west-2.amazonaws.com/3359003228bf403889ef173492370bec.png", birthday=date(1980, 12, 31))
     ]
 
     db.session.add_all(users)
